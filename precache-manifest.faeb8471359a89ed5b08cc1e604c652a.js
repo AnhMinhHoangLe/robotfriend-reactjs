598f8e857221b9e9008ae787114c85798d6257fa
AnhMinhHoangLe <leanh6998@gmail.com>
2020-06-14 22:51:55 -0700

    Updates

---
+++ precache-manifest.faeb8471359a89ed5b08cc1e604c652a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3453b0a6711f98d7bdc65ed433085b7",
+    "url": "/robotfriend-reactjs/index.html"
+  },
+  {
+    "revision": "1083cc04a724b80141f1",
+    "url": "/robotfriend-reactjs/static/css/2.c6861289.chunk.css"
+  },
+  {
+    "revision": "cdd356565c4b4d337e16",
+    "url": "/robotfriend-reactjs/static/css/main.2fe735a6.chunk.css"
+  },
+  {
+    "revision": "1083cc04a724b80141f1",
+    "url": "/robotfriend-reactjs/static/js/2.960d5bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriend-reactjs/static/js/2.960d5bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd356565c4b4d337e16",
+    "url": "/robotfriend-reactjs/static/js/main.103b4eac.chunk.js"
+  },
+  {
+    "revision": "8ce6b45f1a807fbb9c1a",
+    "url": "/robotfriend-reactjs/static/js/runtime-main.07b3b493.js"
+  }
+]);
